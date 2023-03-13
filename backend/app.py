f422c1e0042a60c39137201eb75ca8761f90fff2
Junjie Jin <junjie_j3@163.com>
2023-03-13 10:46:56 +0800

    feat(backend): fix bug

--- backend/app.py
+++ backend/app.py
@@ -947,7 +947,10 @@ def dataGen(json):
       num_len = format['length']
       columns = format['children']
       col_size = format['column']
-      sort_config = format['order']
+      if 'order' in format:
+        sort_config = format['order']
+      else:
+        sort_config = []
       
       output = []
       cnt = 0
